734537a927c56fb06f927df31ce599fadc730cce
Daria <spodina.darya@gmail.com>
2022-11-02 21:01:25 +0100

    updates

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
 // Task 1
-const button = document.querySelector(".button");
-button.onclick = function () {
-  fetch("becode.json")
-    .then((j) => j.json())
-    .then((data) => renderList(data));
-};
-function renderList(list) {
-  const ul = document.createElement("ul");
-  document.body.append(ul);
-  list.forEach((rule) => {
-    const li = document.createElement("li");
-    li.innerText = rule;
-    ul.append(li);
-  });
-}
+// const button = document.querySelector(".button");
+// button.onclick = function () {
+//   fetch("becode.json")
+//     .then((j) => j.json())
+//     .then((data) => renderList(data));
+// };
+// function renderList(list) {
+//   const ul = document.createElement("ul");
+//   document.body.append(ul);
+//   list.forEach((rule) => {
+//     const li = document.createElement("li");
+//     li.innerText = rule;
+//     ul.append(li);
+//   });
+// }
 
 // Task 2
 const button = document.querySelector(".button");
-const input = document.querySelector('input')
+const input = document.querySelector("input");
+const select = document.querySelector("select");
 button.onclick = function () {
-    let name = input.value;
-    getAge (name)
-}
+  let name = input.value;
+  let country = select.value;
+  input.value = "";
+  select.value = "";
+  if (name) {
+    getAge(name, country);
+  }
+};
 const ul = document.createElement("ul");
-  document.body.append(ul);
-function getAge (name){
-    fetch(`https://api.agify.io?name=${name}`)
-    .then((j) => j.json())
-    .then((data) => {
-        const li = renderAge(data.age)
-        ul.append(li);
-    });
+document.body.append(ul);
+async function getAge(name, country = "") {
+  let j = await fetch(
+    `https://api.agify.io?name=${name}${
+      country ? `&country_id=${country}` : ""
+    }`
+  )
+    // .then((j) => j.json())
+    let data = await j.json()
+    // .then((data) => {
+      let text = `${data.name} has ${data.age}`;
+      setStorage("ages", text);
+      const li = renderAge(text);
+      ul.append(li);
+    // });
 }
-function renderAge(age) {
-    const li = document.createElement("li");
-    li.innerText = age;
-    return li;
+function renderAge(text) {
+  const li = document.createElement("li");
+  li.innerText = text;
+  return li;
 }
+function setStorage(key, data) {
+  let previousValue = localStorage.getItem(key);
+  let value = [];
+  if (previousValue) {
+    value = JSON.parse(previousValue);
+  }
+  value.push(data);
+  localStorage.setItem(key, JSON.stringify(value));
+};
+(()=>{
+    let previousValue = localStorage.getItem('ages');
+    if (previousValue){
+        JSON.parse(previousValue).forEach(element => {
+            const li = renderAge(element);
+            ul.append(li);  
+        });
+    }
+})();
